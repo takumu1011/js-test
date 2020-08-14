280f56d26f5e66e4528bde37f8b71b452a536d67
tkm <sumikawa.web@gmail.com>
2020-08-14 22:09:35 +0900

    location

---
+++ location/script.js
@@ -0,0 +1,6 @@
+// location.href = 'https://google.com';
+console.log(location.host);
+location.hash = 'hogehoge';
+location.search = 'fugafuga';
+location.pathname = 'assets/js/';
+location.protocol = 'https';
