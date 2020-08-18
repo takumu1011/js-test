b0363733082170e6e5577a31f8e50cede591f973
tkm <sumikawa.web@gmail.com>
2020-08-18 22:19:21 +0900

    array operation

---
+++ array-ope/script.js
@@ -0,0 +1,29 @@
+const arry = ['hoge', 'fuga', 'bar', 'baz'];
+
+console.log(arry.length);
+console.log(arry.splice(1, 2));
+console.log(arry);
+
+const arry2 = [1, 2, 3, 4, 5];
+
+console.log(arry2.reverse());
+const arry3 = [6, 7, 8];
+const arry4 = [9, 10];
+
+console.log(arry2.concat(arry3, arry4));
+
+const arry5 = ['sleep', 'eat', 'work'];
+
+console.log(arry5.join(' and '));
+console.log(arry5);
+
+const arry6 = ['one', 'two', 'three', 'four', 'five'];
+console.log(arry6.slice(1, 4));
+console.log(arry6);
+
+const arry7 = ['ラーメン', 'うどん', 'そば'];
+console.log(arry7.sort());
+const arry8 = [2, 100, 10, 8, 'good', 'おはよう'];
+console.log(arry8.sort());
+const arry9 = [2, 100, 10, 8];
+console.log(arry9.sort());
