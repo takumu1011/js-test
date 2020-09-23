03dc6357ede2e3ce90389c0f2fbbd7d805074349
tkm <sumikawa.web@gmail.com>
2020-09-23 22:31:11 +0900

    trim

---
+++ trim/script.js
@@ -0,0 +1,15 @@
+let hoge = '';
+console.log(hoge.trim());
+console.log(!hoge.trim());
+
+let fuga = '　あいうえお　';
+console.log(fuga.trim());
+console.log(!fuga.trim());
+
+let bar = 'アイウエオ';
+console.log(bar.trim());
+console.log(!bar.trim());
+
+let baz = '　あ　い　う　え　お';
+console.log(baz.trim());
+console.log(!baz.trim());
